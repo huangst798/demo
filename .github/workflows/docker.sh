f01992e151fd1bc165ac92c06bcc7baf59b6e966
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-08 19:57:39 +0800

    docker6

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -1,1 +1,7 @@
-echo "ssssss"
+#!/bin/bash
+image_name='myhello:2.0'
+image_id='1104437281'
+docker build -t ${image_name} .
+docker run ${image_name}
+docker image tag ${image_name} ${image_id}/${image_name}
+docker image push ${image_id}/${image_name}
