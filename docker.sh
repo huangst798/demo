d8100498824ee9c1b4feb325f905e8b3cbc9cb2b
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-08 20:41:11 +0800

    docker13

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 image_name='myhello:6.0'
 image_id='1104437281'
+image_pwd='hst7562336'
+docker login -u ${image_id} --password-${image_pwd}
 docker build -t ${image_name} .
 docker run ${image_name}
 docker image tag ${image_name} ${image_id}/${image_name}
