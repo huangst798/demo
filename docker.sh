2f25c078b4472f274da4e74679814e906ac37a1e
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-09 11:33:53 +0800

    bulid5

--- docker.sh
+++ docker.sh
@@ -9,7 +9,7 @@ docker login -u ${image_id} --password ${image_pwd}
 # docker run ${image_name}
 # docker image tag ${image_name} ${image_id}/${image_name}
 # docker image push ${image_id}/${image_name}
-
+docker buildx create --use
 docker buildx build \
         --no-cache \
         --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
