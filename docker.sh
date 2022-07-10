9f72ff0560f5f87f6ec7e8b84c9466fe1194ccc1
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-10 20:11:46 +0800

    bulid20

--- docker.sh
+++ docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-image_name='myhello:6.0'
 image_id='1104437281'
 image_pwd='hst7562336'
-FILE='src/arch/2-dev.Dockerfile'
-IMAGE='2-dev'
 docker login -u ${image_id} --password ${image_pwd}
 # docker build -t ${image_name} .
 # docker run ${image_name}
 # docker image tag ${image_name} ${image_id}/${image_name}
 # docker image push ${image_id}/${image_name}
+for FILE in `ls ./src/*`
+do
+    echo "${FILE}"
+done
 docker buildx create --use
 docker buildx build \
         --no-cache \
---
+++ test.sh
@@ -0,0 +1,5 @@
+for file in `ls ./src/*`
+do
+    image="image"
+    echo "${file} ${image}"
+done
