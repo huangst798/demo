22e321b69f81fc5048616d914b129ff531aa558b
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-11 13:58:12 +0800

    bulid25

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
-for sys in alpine arch bash debian devc fedora gentoo opensuse ubuntu centos ; do
-        for dfile in $(ls ./src/$sys); do
-        IMAGE==sys
-            echo ./src/$sys/$dfile ${IMAGE}
-    done 
-done
+#!/bin/bash
+image_id='1104437281'
+image_pwd='hst7562336'
+docker login -u ${image_id} --password ${image_pwd}
+FILE='1-curl.Dockerfile'
+IMAGE='1-curl'
+# docker build -t ${image_name} .
+# docker run ${image_name}
+# docker image tag ${image_name} ${image_id}/${image_name}
+# docker image push ${image_id}/${image_name}
+        docker buildx create --use
+        docker buildx build \
+        --no-cache \
+        --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
+        -f "$FILE" \
+        -t "$image_id/$IMAGE" . --push
