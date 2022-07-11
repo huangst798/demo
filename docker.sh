8f6cd8d906d9de7638c693450618ba0a21be2b76
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-11 09:25:57 +0800

    bulid24

--- docker.sh
+++ docker.sh
@@ -6,15 +6,15 @@ IMAGE="image"
 for sys in alpine arch bash debian devc fedora gentoo opensuse ubuntu centos ; do 
     for FIlE in $(ls ./src/$sys); do
         echo ./src/$sys/$FIlE
+        docker buildx create --use
+        docker buildx build \
+        --no-cache \
+        --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
+        -f "$FILE" \
+        -t "$image_id/$IMAGE" . --push
     done 
 done
 # docker build -t ${image_name} .
 # docker run ${image_name}
 # docker image tag ${image_name} ${image_id}/${image_name}
 # docker image push ${image_id}/${image_name}
-docker buildx create --use
-docker buildx build \
-        --no-cache \
-        --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
-        -f "$FILE" \
-        -t "$image_id/$IMAGE" . --push
