a557859a6289a094fdd8f0a623b5d51dfd6f070d
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-11 14:16:03 +0800

    ubuntu bulid1

--- docker.sh
+++ docker.sh
@@ -2,15 +2,14 @@
 image_id='1104437281'
 image_pwd='hst7562336'
 docker login -u ${image_id} --password ${image_pwd}
-FILE='./src/debian/1-curl.9.Dockerfile'
-IMAGE='1-curl.9'
-# docker build -t ${image_name} .
-# docker run ${image_name}
-# docker image tag ${image_name} ${image_id}/${image_name}
-# docker image push ${image_id}/${image_name}
+for IMAGE in ubuntu; do 
+    for FIlE in $(ls ./src/$IMAGE); do
+        echo ./src/$IMAGE/$FIlE
         docker buildx create --use
         docker buildx build \
         --no-cache \
         --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
-        -f "$FILE" \
+        -f "./src/$IMAGE/$FILE" \
         -t "$image_id/$IMAGE" . --push
+    done 
+done
--- test.sh
+++ test.sh
@@ -14,3 +14,23 @@ IMAGE='1-curl'
         --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
         -f "$FILE" \
         -t "$image_id/$IMAGE" . --push
+
+        #!/bin/bash
+image_id='1104437281'
+image_pwd='hst7562336'
+docker login -u ${image_id} --password ${image_pwd}
+for IMAGE in ubuntu; do 
+    for FIlE in $(ls ./src/$IMAGE); do
+        echo ./src/$IMAGE/$FIlE
+        docker buildx create --use
+        docker buildx build \
+        --no-cache \
+        --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
+        -f "./src/$IMAGE/$FILE" \
+        -t "$image_id/$IMAGE" . --push
+    done 
+done
+# docker build -t ${image_name} .
+# docker run ${image_name}
+# docker image tag ${image_name} ${image_id}/${image_name}
+# docker image push ${image_id}/${image_name}
