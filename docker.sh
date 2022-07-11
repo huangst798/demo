6543f24dd22fe4e8d331a08cdb3f44b6c796d49d
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-11 14:37:11 +0800

    alpine bulid3

--- docker.sh
+++ docker.sh
@@ -2,10 +2,9 @@
 image_id='1104437281'
 image_pwd='hst7562336'
 docker login -u ${image_id} --password ${image_pwd}
-for sys in alpine; do 
-        for file in $(ls ./src/$sys); do   
-        FILE=$( echo ./src/$sys/$file )
-        IMAGE=$( echo $file)
+for IMAGE in alpine; do 
+        for file in $(ls ./src/$IMAGE); do   
+         FILE=$( echo ./src/$IMAGE/$file )   
         echo $FIlE
         docker buildx create --use
         docker buildx build \
