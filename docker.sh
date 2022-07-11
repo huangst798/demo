a9e90e0da43161ab1a9906e264b6b10dc76d0909
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-11 14:02:57 +0800

    bulid26

--- docker.sh
+++ docker.sh
@@ -2,18 +2,15 @@
 image_id='1104437281'
 image_pwd='hst7562336'
 docker login -u ${image_id} --password ${image_pwd}
-for IMAGE in ubuntu; do 
-    for FIlE in $(ls ./src/$IMAGE); do
-        echo ./src/$IMAGE/$FIlE
+FILE='1-curl.Dockerfile'
+IMAGE='1-curl'
+# docker build -t ${image_name} .
+# docker run ${image_name}
+# docker image tag ${image_name} ${image_id}/${image_name}
+# docker image push ${image_id}/${image_name}
         docker buildx create --use
         docker buildx build \
         --no-cache \
         --platform "${PLATFORM:-linux/arm,linux/arm64,linux/amd64}" \
         -f "$FILE" \
         -t "$image_id/$IMAGE" . --push
-    done 
-done
-# docker build -t ${image_name} .
-# docker run ${image_name}
-# docker image tag ${image_name} ${image_id}/${image_name}
-# docker image push ${image_id}/${image_name}
