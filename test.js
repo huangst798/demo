e180f2e5d59211e0a9e78fe94a196c2f86382948
huangshengtao <11092596+huangshengtao798@user.noreply.gitee.com>
2022-07-12 20:18:50 +0800

    定时任务

---
+++ test.js
@@ -0,0 +1,58 @@
+{
+    "msgtype":"template_card",
+    "template_card":{
+        "card_type":"news_notice",
+        "main_title":{
+            "title":"夜静渐深，让我们进入打字训练。"
+        }，
+        "card_image":{
+            "url":"https://wework.qpic.cn/wwpic/354393_4zpkKXd7SrGMvfg_1629280616/0",
+            "aspect_ratio":2.25
+        },  
+        "card_action":{
+            "type":1,
+            "url":"https://work.weixin.qq.com/?from=openApi",
+            "appid":"APPID",
+            "pagepath":"PAGEPATH"
+        } 
+    }
+}
+
+
+
+
+{
+    "msgtype": "news",
+    "news": {
+       "articles" :{
+               "title" : "夜静渐深，让我们进入打字训练",
+               "picurl" : "http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png"
+           }
+        "horizontal_content_list":[
+            {
+                "keyname":"英文打字练习",
+                "value":"英文训练",
+                "type":1,
+                "url":"https://work.weixin.qq.com/?from=openApi"
+            },
+            {
+                "keyname":"双拼打字练习",
+                "value":"双拼训练",
+                "type":1,
+                "url":"https://work.weixin.qq.com/?from=openApi"
+            }
+        ]
+    }
+}
+
+
+
+
+{
+    "msgtype": "markdown",
+    "markdown": {
+        "content": "夜静渐深，让我们进入打字训练。\n
+         >英文打字练习:<font color=\"comment\">英文练习</font>\n
+         >双拼打字练习:<font color=\"comment\">中文练习</font>"
+    }
+}
